4b43dd7ce7dd917fdc4628d2feac48c9f76c07dd
JuliaLopesCoimbra <juliacristinalopes2607@gmail.com>
2024-12-05 14:06:27 -0300

    aa

--- web/src/components/SideMenu/ChatMessage/ChatMessage.js
+++ web/src/components/SideMenu/ChatMessage/ChatMessage.js
@@ -9,21 +9,23 @@ import Avatar from "../../../assets/Avatar";
 //user user or gpt
 //message - vai estar o prompt
 export const ChatMessage = ({ message }) => {
-  <div className=
-    {`chat-message ${message.user === 'gpt'} && "chatgpt"`}
-  >
-    <div className="chat-message-center">
-      <div className={`avatar ${message.user === 'gpt' && 'gpt' && "chatgpt"}`}>
-        {message.user === 'gpt' && (
-          <Avatar />
-        )}
+  return (
+    <div className=
+      {`chat-message ${message.user === 'gpt'} && "chatgpt"`}
+    >
+      <div className="chat-message-center">
+        <div className={`avatar ${message.user === 'gpt' && 'gpt' && "chatgpt"}`}>
+          {message.user === 'gpt' && (
+            <Avatar />
+          )}
+        </div>
+
+        <div className="message">
+          {message.message}
+
+        </div>
+
       </div>
-
-      <div className="message">
-        {message.message}
-
-      </div>
-
     </div>
-  </div>
+  )
 }
